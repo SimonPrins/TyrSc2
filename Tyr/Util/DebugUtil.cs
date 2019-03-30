010ad27713dcaa7d8bdeece0a003eedd9a10057c
Simon Prins <simon.prins@xs4all.nl>
2019-03-30 20:27:12 +0100

    Added util classes.

---
+++ Tyr/Util/DebugUtil.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Tyr.Util
+{
+    public class DebugUtil
+    {
+        // Writing to the console doesn't work on linux when the output stream is closed.
+        // Here we check if that is the case and if so we stop writing to the console.
+        private static bool ConsoleBroken = false;
+
+        public static void WriteLine(string line)
+        {
+            if (!ConsoleBroken)
+            {
+                try
+                {
+                    Console.WriteLine(line);
+                }
+                catch (Exception)
+                {
+                    ConsoleBroken = true;
+                }
+            }
+        }
+
+        public static void WriteLine()
+        {
+            WriteLine("");
+        }
+    }
+}
---
+++ Tyr/Util/FileUtil.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.IO;
+
+namespace Tyr.Util
+{
+    public class FileUtil
+    {
+        private static string TournamentFile;
+        private static string ResultsFile;
+        private static string DataFolder = Directory.GetCurrentDirectory() + "/data/Tyr/";
+        private static string LogFile;
+        private static string DebugFile;
+        private static string BuildFile = AppDomain.CurrentDomain.BaseDirectory + "build.txt";
+        public static bool AllowWritingFiles = true;
+
+        public static void LogTournament(string line)
+        {
+            InitializeTournamentFile();
+            File.AppendAllLines(TournamentFile, new string[] { line });
+        }
+
+        public static void Log(string line)
+        {
+            InitializeLogFile();
+            if (AllowWritingFiles)
+                File.AppendAllLines(LogFile, new string[] { line });
+        }
+
+        public static void Debug(string line)
+        {
+            InitializeDebugFile();
+            if (AllowWritingFiles && Tyr.Debug)
+                File.AppendAllLines(DebugFile, new string[] { line });
+        }
+
+        public static void Register(string line)
+        {
+            InitializeResultsFile();
+            if (AllowWritingFiles)
+                File.AppendAllLines(ResultsFile, new string[] { line });
+        }
+
+        public static string[] ReadTournamentFile()
+        {
+            InitializeTournamentFile();
+            return File.ReadAllLines(TournamentFile);
+        }
+
+        public static string[] ReadResultsFile()
+        {
+            InitializeResultsFile();
+            return File.ReadAllLines(ResultsFile);
+        }
+
+        public static string[] ReadBuildFile()
+        {
+            if (!File.Exists(BuildFile))
+                return new string[0];
+
+            return File.ReadAllLines(BuildFile);
+        }
+
+        private static void InitializeResultsFile()
+        {
+            if (ResultsFile == null)
+            {
+                if (Tyr.Bot.OpponentID == null)
+                    ResultsFile = DataFolder + Tyr.Bot.EnemyRace + ".txt";
+                else
+                    ResultsFile = DataFolder + Tyr.Bot.OpponentID + ".txt";
+
+                if (AllowWritingFiles && !File.Exists(ResultsFile))
+                {
+                    Directory.CreateDirectory(DataFolder);
+                    File.Create(ResultsFile).Close();
+                }
+            }
+        }
+
+        private static void InitializeLogFile()
+        {
+            if (LogFile == null)
+            {
+                LogFile = DataFolder + "Tyr.log";
+                if (AllowWritingFiles && !File.Exists(LogFile))
+                {
+                    Directory.CreateDirectory(DataFolder);
+                    File.Create(LogFile).Close();
+                }
+            }
+        }
+
+        private static void InitializeTournamentFile()
+        {
+            if (TournamentFile == null)
+            {
+                TournamentFile = DataFolder + "tounament.txt";
+                if (!File.Exists(TournamentFile))
+                {
+                    Directory.CreateDirectory(DataFolder);
+                    File.Create(TournamentFile).Close();
+                }
+            }
+        }
+
+        private static void InitializeDebugFile()
+        {
+            if (DebugFile == null)
+            {
+                DebugFile = DataFolder + "debug.txt";
+                if (AllowWritingFiles && !File.Exists(DebugFile))
+                {
+                    Directory.CreateDirectory(DataFolder);
+                    File.Create(DebugFile).Close();
+                }
+            }
+        }
+    }
+}
