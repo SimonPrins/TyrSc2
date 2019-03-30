00dba4e74fb07c85a27811333de46d4c2cd6597e
Simon Prins <simon.prins@xs4all.nl>
2019-03-30 20:42:44 +0100

    Added additional information to Base class.

--- Tyr/Managers/Base.cs
+++ Tyr/Managers/Base.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using SC2APIProtocol;
+using System.Collections.Generic;
 using Tyr.Agents;
 using Tyr.MapAnalysis;
 
@@ -8,10 +9,16 @@ namespace Tyr.Managers
     {
         public BaseLocation BaseLocation { get; set; }
         public Agent ResourceCenter { get; set; }
+        public int ResourceCenterFinishedFrame = -1;
         public int Owner { get; set; }
         public Dictionary<uint, int> BuildingCounts = new Dictionary<uint, int>();
         public Dictionary<uint, int> BuildingsCompleted = new Dictionary<uint, int>();
         public int DistanceToMain { get; set; }
         public bool UnderAttack;
+        public bool Blocked;
+        public Point2D MineralLinePos;
+        public Point2D OppositeMineralLinePos;
+        public Point2D MineralSide1;
+        public Point2D MineralSide2;
     }
 }
